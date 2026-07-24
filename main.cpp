8abeeb3ffe2315d0ef269da99a462073cd1e9b43
Nadane Stefan <1stefan20.10nd@gmail.com>
2026-07-24 17:25:34 +0300

    Convert menu to switch statement and numeric menu system

--- main.cpp
+++ main.cpp
@@ -16,73 +16,144 @@ void printTasks(std::vector<Task> tasks) {
     }
 }
 
-int main() {
-    TodoManager manager("tasks.txt");
-    std::string comanda;
-
-    std::cout << "Todo App - comenzi: list, list-priority, list-alpha, list-done, add, done, del, filter-priority, filter-done, quit" << std::endl;
-
-    while(true) {
+void meniuAfisare(TodoManager &manager) {
+    while (true) {
+        std::cout << "\n-- Afisare --" << std::endl;
+        std::cout << "1. Toate task-urile" << std::endl;
+        std::cout << "2. Sortare dupa prioritate" << std::endl;
+        std::cout << "3. Sortare dupa status" << std::endl;
+        std::cout << "4. Sortare alfabetica" << std::endl;
+        std::cout << "0. Inapoi" << std::endl;
         std::cout << "> ";
-        std::cin >>comanda;
 
-        if(comanda == "list") {
+        int optiune;
+        std::cin >> optiune;
+
+        /*if (optiune == 1) {
             printTasks(manager.getAll());
+        } else if (optiune == 2) {
+            printTasks(manager.getAllSortedByPriority());
+        } else if (optiune == 3) {
+            printTasks(manager.getAllSortedByDone());
+        } else if (optiune == 4) {
+            printTasks(manager.getAllSortedAlpha());
+        } else if (optiune == 0) {
+            return;
+        } else {
+            std::cout << "Optiune necunoscuta." << std::endl;
+        }*/
 
-        } else if (comanda == "add") {
-            std::string title, description;
-            int priority;
-            std::cout << "Titlu: ";
-            std::cin.ignore();
-            std::getline(std::cin, title);
-            std::cout << "Descriere: ";
-            std::getline(std::cin, description);
-            std::cout << "Prioritate (1-3): ";
-            std::cin >> priority;
-            manager.addTask(title, description, priority);
-            std::cout << "Task adaugat." << std::endl;
+        switch (optiune) {
+            case 1:
+                printTasks(manager.getAll());
+                break;
+            case 2:
+                printTasks(manager.getAllSortedByPriority());
+                break;
+            case 3:
+                printTasks(manager.getAllSortedByDone());
+                break;
+            case 4:
+                printTasks(manager.getAllSortedAlpha());
+                break;
+            case 0:
+                return;
+            default:
+                std::cout << "Optiune necunoscuta." << std::endl;
+        }
+    }
+}
 
-        } else if (comanda == "done") {
-            int id;
-            std::cout << "ID task: ";
-            std::cin >> id;
-            manager.completeTask(id);
-            std::cout << "Task marcat ca finalizat." << std::endl;
+void meniuFiltrare(TodoManager &manager) {
+    while (true) {
+        std::cout << "\n-- Filtrare --" << std::endl;
+        std::cout << "1. Dupa prioritate" << std::endl;
+        std::cout << "2. Dupa status" << std::endl;
+        std::cout << "0. Inapoi" << std::endl;
+        std::cout << "> " << std::endl;
 
-        } else if (comanda == "del") {
-            int id;
-            std::cout << "ID task: ";
-            std::cin >> id;
-            manager.deleteTask(id);
-            std::cout << "Task sters." << std::endl;
-        
-        } else if (comanda == "filter-priority") {
+        int optiune;
+        std::cin >> optiune;
+
+        switch (optiune)
+        {
+        case 1:
             int priority;
             std::cout << "Prioritate (1-3): ";
             std::cin >> priority;
             printTasks(manager.getByPriority(priority));
-        
-        } else if (comanda == "filter-done") {
+            break;
+        case 2:
             bool done;
             std::cout << "Rezolvate (1) sau nerezolvate (0): ";
             std::cin >> done;
             printTasks(manager.getByDone(done));
-
-        } else if (comanda == "list-priority") {
-            printTasks(manager.getAllSortedByPriority());
-
-        } else if (comanda == "list-alpha") {
-            printTasks(manager.getAllSortedAlpha());
-
-        } else if (comanda == "list-done") {
-            printTasks(manager.getAllSortedByDone());
-
-        } else if (comanda == "quit") {
             break;
+        case 0:
+            return;
+        default:
+            std::cout << "Optiune necunoscuta." << std::endl;
+        }
+    }
+}
 
-        } else {
-            std::cout << "Comanda necunoscuta." << std::endl;
+int main() {
+    TodoManager manager("tasks.txt");
+    
+    while(true) {
+        std::cout << "\n=== Todo App ===" << std::endl;
+        std::cout << "1. Afisare" << std::endl;
+        std::cout << "2. Filtare" << std::endl;
+        std::cout << "3. Adauga task" << std::endl;
+        std::cout << "4. Marcheaza task-ul ca rezolvat" << std::endl;
+        std::cout << "5. Sterge task" << std::endl;
+        std::cout << "0. Iesire" << std::endl;
+        std::cout << "> ";
 
+        int optiune;
+        std::cin >> optiune;
+
+        switch (optiune) {
+            case 1:
+                meniuAfisare(manager);
+                break;
+            case 2:
+                meniuFiltrare(manager);
+                break;
+            case 3: {    
+                std::string title, description;
+                int priority;
+                std::cout << "Titlu: ";
+                std::cin.ignore();
+                std::getline(std::cin, title);
+                std::cout << "Descriere: ";
+                std::getline(std::cin, description);
+                std::cout << "Prioritate (1-3): ";
+                std::cin >> priority;
+                manager.addTask(title, description, priority);
+                std::cout << "Task adaugat." << std::endl;
+                break;
+            }
+            case 4: {
+                int id;
+                std::cout << "ID task: ";
+                std::cin >> id;
+                manager.completeTask(id);
+                std::cout << "Task marcat ca rezolvat." << std::endl;
+                break;
+            }
+            case 5: {
+                int id;
+                std::cout << "ID task: ";
+                std::cin >> id;
+                manager.deleteTask(id);
+                std::cout << "Task sters." << std::endl;
+                break;
+            }
+            case 0:
+                return 0;
+            default:
+                std::cout << "Optiune necunoscuta." << std::endl;
         }
     }
 
